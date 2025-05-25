44915fdd8872321643c0486be57dce86f0957a1b
andyladera <andcalizaya@upt.pe>
2025-05-25 17:02:31 -0500

    PASO10_OK

---
+++ MyMath/Math.Lib/Rooter.cs
@@ -0,0 +1,10 @@
+namespace Math.Lib //Espacio de nombre que contiene clases y métodos
+{
+    public class Rooter //Nombre de la clase
+    {
+        public double SquareRoot(double input) //Método
+        {
+            return input / 2;
+        }
+    }
+}
---
+++ MyMath/Math.Tests/RooterTests.cs
@@ -0,0 +1,24 @@
+//RooterTests.cs es un archivo de prueba unitaria utilizando MSTest
+// ---
+//Espacio de nombres para poder usar las clases y atributos de MSTest
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using Math.Lib;
+
+namespace Math.Tests //Nombre del namespace
+{
+    [TestClass] //Indica que la clase contiene métodos de prueba
+    public class RooterTests //Clase de prueba
+    {
+        [TestMethod] //Indica que el siguiente método es una prueba unitaria individual ejecutada por MSTest
+        public void BasicRooterTest()
+        {
+            Rooter rooter = new Rooter(); //Instancia de clase Rooter
+            double expectedResult = 2.0; //valor esperado
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input); //Llama al método SqueareRoot con el valor 4.0
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100); //Verifica que expectedResult sea igual a actualResult
+                                                                                        //dentro un margen de error delta
+                                                                                        //margen de error 2.0/100 = 0.02
+        }
+    }
+}
