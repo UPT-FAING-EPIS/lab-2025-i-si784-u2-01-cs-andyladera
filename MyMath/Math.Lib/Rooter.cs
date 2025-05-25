2a433db89ac281577ae7abbf8ef9e611eeee0df6
andyladera <andcalizaya@upt.pe>
2025-05-25 17:20:46 -0500

    PASO18OK

--- MyMath/Math.Lib/Rooter.cs
+++ MyMath/Math.Lib/Rooter.cs
@@ -4,7 +4,16 @@ namespace Math.Lib //Espacio de nombre que contiene clases y métodos
     {
         public double SquareRoot(double input) //Método
         {
-            return input / 2;
+            if (input <= 0.0) throw new ArgumentOutOfRangeException();
+            double result = input;
+            double previousResult = -input;
+            while (System.Math.Abs(previousResult - result)
+                > result / 1000)
+            {
+            previousResult = result;
+            result = result - (result * result - input) / (2 * result);
+            }
+            return result;
         }
     }
 }
